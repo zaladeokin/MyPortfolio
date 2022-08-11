3b7c84cdf3b47bc1b3993a017af9375ab5ddecc6
Zacchaeus Aladeokin <zaladeokin@gmail.com>
2022-08-11 19:31:57 +0100

    Header UI designed.

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Webnesis</title>
+        <meta name="keywords" content="website, webpage, development, developer,webnesis, developer" />
+        <meta name="author" content="Zacchaeus Aladeokin" />
+        <meta name="description" content="This webpage is where you can know about me as your web developer and view my works and certification." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="\MyPortfolio\css\header.css" />
+    </head>
+    <body>
+        <header>
+        <div id="header">
+           <img class="hLogo" src="\MyPortfolio\images\logo.png" />
+           <div>WEBNESIS</div>
+        </div>
+</header>
+<?php
+/* 
+Remember to close the below tag in footer.php
+    </body>
+</html>
+*/
+?>
