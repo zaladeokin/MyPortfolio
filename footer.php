452c2e2e278d5d409bc5b50e82c54954fc1e2759
Zacchaeus Aladeokin <zaladeokin@gmail.com>
2022-08-15 19:31:39 +0100

    Index.php designed with responsiveness using Bootstrap.

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
-<footer>
-    <div>
-    &copy; <?php echo date("Y", time());?> WEBNESIS  <img src="\MyPortfolio\images\icon.png" />
+<div class="row">
+    <div clas="col-12">
+        <footer>
+            <div>
+                &copy; <?php echo date("Y", time());?> WEBNESIS  <img src="\MyPortfolio\images\icon.png" />
+            </div>
+        </footer>
     </div>
-</footer>
+</div>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Webnesis</title>
         <meta name="keywords" content="website, webpage, development, developer,webnesis, developer" />
         <meta name="author" content="Zacchaeus Aladeokin" />
         <meta name="description" content="This webpage is where you can know about me as your web developer and view my works and certification." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="\MyPortfolio\css\header.css" />
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="myStyle/header.css">
+        <link rel="stylesheet" href="myStyle/body.css">
+        <link rel="stylesheet" href="myStyle/footer.css">
+        
+        <!--<link rel="stylesheet" type="text/css" href="\MyPortfolio\css\header.css" />
         <link rel="stylesheet" type="text/css" href="\MyPortfolio\css\body.css" />
-        <link rel="stylesheet" type="text/css" href="\MyPortfolio\css\footer.css" />
-    </head>
-    <body>
-        <header>
-        <div id="header">
-           <img class="hLogo" src="\MyPortfolio\images\logo.png" />
-           <div>WEBNESIS</div>
-        </div>
-</header>
+        <link rel="stylesheet" type="text/css" href="\MyPortfolio\css\footer.css" /> -->
+        </head>
+<body>
+    <div class="row" id="header">
+        <div class="col"><img class="hLogo" src="\MyPortfolio\images\logo.png" /><span id="sHeading">WEBNESIS</span></div>
+        <div class="col-2">NAV</div>
+      </div>
 <?php
 /* 
 Remember to close the below tag in footer.php
