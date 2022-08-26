ad03d72dec669b02b08c18cc31f2dd1bb99af41a
Zacchaeus Aladeokin <zaladeokin@gmail.com>
2022-08-26 10:04:49 +0100

    Index page check to portal for signing in and home .php was created

--- Admin/index.php
+++ Admin/index.php
@@ -1,12 +1,16 @@
 <?php
 include("header.php");
 ?>
-<div class="container-fluid p-5 fs-4">
-    <h1> Administration Page</h1><br>
-<ul>
-<li><a href="Uproject.php">Upload Project work.</a></li>
-<li><a href="Ucertificate.php">Upload Certificate</a></li>
-</ul>
+
+<div class="container p-5 fs-4">
+    <h1> Portal</h1><br>
+<form class="fs-4">
+    <!-- Login form, PHP should generate hash for this page to be access-->
+            <label for="user">User  </label>&nbsp;&nbsp;
+            <input type="text" class="form-control" /> <br />
+            <label for="password">Password  </label>&nbsp;&nbsp;
+            <input type="password" class="form-control" /> <br />
+</form>
 <br />
 </div>
 
