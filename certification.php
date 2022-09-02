b931181e558dd05ba1b9c3bc36e086046808d476
Zacchaeus Aladeokin <zaladeokin@gmail.com>
2022-09-02 13:13:35 +0100

    certificated.php back-end developed

--- certification.php
+++ certification.php
@@ -1,89 +1,65 @@
 <?php
 require('header.php');
+
+$data= query("SELECT * FROM Certificate");
+$cont= ($data->num_rows) > 0 ? true : false; //check the number of work upload, 0 means no work
 ?>
-  <!--  ******** Alternate contentent to generate if there's no Certificate**********************
 
-    <div class="row fs-4" style="padding:1em;">
-      <div class="col-sm-12">
-        <div class="alert alert-info">
-          <h1> Certification.</h1><br />
-          <p> Sorry, Certification  not available.<br /> 
+<?php
+//View start here...............................
+
+if(!$cont){
+  //******** Alternate content to generate if there's no Certificate**********************
+    echo <<<_END
+      <div class="row fs-4" style="padding:1em;">
+        <div class="col-sm">
+          <div class="alert alert-info">
+            <h1> Certification.</h1><br />
+            <p> Sorry, Certification  not available.<br /> 
           Check back later.</p>
-        </div> 
-    </div>
-  </div>
-
-  -->
-  <div class="row fs-4" style="padding:1em;">
-    <div class="col-sm-12">
-      <h1> Certification.</h1><br />
-      <p> The following certify the skills acquired that I used as my development tools, You can verify them through the link that was provided underneath.</p>
-    </div>
-  </div>
-  <!-- PHP will loop through certificate list here...-->
-
-  <!-- first row (4 col each * 3 images)-->
-  <div class="row fs-4 bg-dark" style="padding:1em;">
-    <div class="col-sm-4 mb-2">
-       <div class="card">
-        <img src="images\certificate\CERTIFICATE_LANDING_PAGE~FFVFFAVPHZ66.jpeg" class="card-image-top" />
-        <div class="card-body">
-          <div class="card-title">Building Web Application in PHP</div>
-          <div class="card-text"><a href="https://coursera.org/verify/FFVFFAVPHZ66"><button class="btn btn-info">Verification Link</button></a></div>
+          </div> 
         </div>
-       </div> 
-    </div>
-    <div class="col-sm-4 mb-2">
-      <div class="card">
-        <img src="images\certificate\CERTIFICATE_LANDING_PAGE~FFVFFAVPHZ66.jpeg" class="card-image-top" />
-        <div class="card-body">
-          <div class="card-title">Building Web Application in PHP</div>
-          <div class="card-text"><a href="https://coursera.org/verify/FFVFFAVPHZ66"><button class="btn btn-info">Verification Link</button></a></div>
+      </div>
+    _END;
+  }else{
+    echo <<<_END
+        <div class="row fs-4" style="padding:1em;">
+          <div class="col-sm-12">
+            <h1> Certification.</h1><br />
+            <p> The following certify the skills acquired that I used as my development tools, You can verify them through the link that was provided underneath.</p>
+          </div>
         </div>
-       </div> 
-    </div>
-    <div class="col-sm-4 mb-2">
-      <div class="card">
-        <img src="images\certificate\CERTIFICATE_LANDING_PAGE~FFVFFAVPHZ66.jpeg" class="card-image-top" />
-        <div class="card-body">
-          <div class="card-title">Building Web Application in PHP</div>
-          <div class="card-text"><a href="https://coursera.org/verify/FFVFFAVPHZ66"><button class="btn btn-info">Verification Link</button></a></div>
-        </div>
-       </div> 
-    </div>
-</div>
+      _END;
+  // Loop through certificate list here...
+  //Rows for display (4 col each * 3 images)-->
+  echo '<div class="row fs-4 bg-dark" style="padding:0.1em;">'; //Begining row <div> here
+  $i=0;
+  while($t = $data->fetch_assoc()){
+    echo <<<_END
+      <div class="col-sm-4 mb-1">
+        <div class="card">
+          <img src="images/certificate/$t[img]" class="card-image-top" />
+          <div class="card-body">
+            <div class="card-title">$t[title]</div>
+            <div class="card-text"><a href="$t[verification]"><button class="btn btn-info">Verification Link</button></a></div>
+          </div>
+         </div> 
+      </div>
+    _END;
+    $i += 1;
+    if($i==3){ 
+        //Additional row <div>
+        echo "</div>";
+        echo '<div class="row fs-4 bg-dark" style="padding:0.1em;">';
+        $i=0;
+        }
+    }
+echo "</div>"; //Ending row <div> here
+  }
+?>
 
-<!-- second row (4 col each * 3 images)-->
-<div class="row fs-4 bg-dark" style="padding:1em;">
-  <div class="col-sm-4 mb-2">
-     <div class="card">
-      <img src="images\certificate\CERTIFICATE_LANDING_PAGE~FFVFFAVPHZ66.jpeg" class="card-image-top" />
-      <div class="card-body">
-        <div class="card-title">Building Web Application in PHP</div>
-        <div class="card-text"><a href="https://coursera.org/verify/FFVFFAVPHZ66"><button class="btn btn-info">Verification Link</button></a></div>
-      </div>
-     </div> 
-  </div>
-  <div class="col-sm-4 mb-2">
-    <div class="card">
-      <img src="images\certificate\CERTIFICATE_LANDING_PAGE~FFVFFAVPHZ66.jpeg" class="card-image-top" />
-      <div class="card-body">
-        <div class="card-title">Building Web Application in PHP</div>
-        <div class="card-text"><a href="https://coursera.org/verify/FFVFFAVPHZ66"><button class="btn btn-info">Verification Link</button></a></div>
-      </div>
-     </div> 
-  </div>
-  <div class="col-sm-4 mb-2">
-    <div class="card">
-      <img src="images\certificate\CERTIFICATE_LANDING_PAGE~FFVFFAVPHZ66.jpeg" class="card-image-top" />
-      <div class="card-body">
-        <div class="card-title">Building Web Application in PHP</div>
-        <div class="card-text"><a href="https://coursera.org/verify/FFVFFAVPHZ66"><button class="btn btn-info">Verification Link</button></a></div>
-      </div>
-     </div> 
-  </div>
-</div>
-<!-- Certificate list end here-->
+
+
 <?php
 require('footer.php');
 ?>
